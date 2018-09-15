0b7bf16ccb8dd8ce58663e60a681270f8f31a265
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-09-15 19:48:53 -0300

    Add dependencies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import styled from "styled-components";
+
+const AppIntro = styled.div`
+  color: red;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p className="App-intro">
+      <BrowserRouter>
+        <AppIntro>
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        </AppIntro>
+      </BrowserRouter>
     );
   }
 }

0f94a78bd5a6893c910210494629bda75d9bda03
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-09-15 20:40:23 -0300

    I18n configuration

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
-import { BrowserRouter } from "react-router-dom";
-import styled from "styled-components";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { I18nextProvider } from "react-i18next";
 
-const AppIntro = styled.div`
-  color: red;
-`;
+import i18n from "./config/i18n";
+import Root from "./root/Root";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <AppIntro>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </AppIntro>
-      </BrowserRouter>
+      <I18nextProvider i18n={i18n}>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/:lang" component={Root} />
+            <Route path="/" component={Root} />
+          </Switch>
+        </BrowserRouter>
+      </I18nextProvider>
     );
   }
 }
--- src/root/Root.js
+++ src/root/Root.js
@@ -1,5 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+import { translate } from "react-i18next";
 
-const Root = () => {
-  return <div />;
-};
+const Container = styled.div`
+  color: blue;
+`;
+
+class Root extends React.Component {
+  componentWillMount() {
+    const { i18n, match } = this.props;
+    if (match.params) {
+      const { lang } = match.params;
+      if (lang) {
+        i18n.changeLanguage(lang);
+      }
+    }
+  }
+
+  render() {
+    const { t } = this.props;
+    return <Container>{t("intro")}</Container>;
+  }
+}
+
+export default translate("root")(Root);

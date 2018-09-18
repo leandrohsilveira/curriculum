fcc5bb55147bca6f27fec5b68bdfc7228ae3ba99
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-09-18 20:28:02 -0300

    Layout component

---
+++ src/app/root/layout/Layout.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  flex: 1;
+  display: flex;
+
+  justify-content: center;
+  padding: 30px;
+  > * {
+    max-width: 1280px;
+  }
+`;
+
+const Paper = styled.div`
+  flex: 1;
+  padding: 30px;
+
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+
+  &:hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
+`;
+
+const Layout = ({ children }) => (
+  <Container>
+    <Paper>{children}</Paper>
+  </Container>
+);
+
+export default Layout;

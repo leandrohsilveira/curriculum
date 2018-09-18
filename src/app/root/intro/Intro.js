ea316e68bda43019a2f64c418e645eb862cd2402
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-09-18 20:43:53 -0300

    Align Intro component children

--- src/app/root/intro/Intro.js
+++ src/app/root/intro/Intro.js
@@ -3,18 +3,23 @@ import styled from "styled-components";
 
 const url = "https://avatars0.githubusercontent.com/u/8015037?s=200&v=4";
 
-const Container = styled.div`
-  color: blue;
+const Container = styled.header`
+  display: flex;
+  align-items: center;
 `;
 
 const Avatar = styled.img`
   border-radius: 50%;
+  margin-right: 15px;
 `;
+
+const Name = styled.h1``;
 
 const Intro = props => {
   return (
     <Container>
       <Avatar src={url} alt="Avatar" />
+      <Name>Leandro Hinckel Silveira</Name>
     </Container>
   );
 };

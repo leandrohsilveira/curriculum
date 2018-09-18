988212826a4ab4659c12ea761525a565ce5fbfe1
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-09-18 20:41:16 -0300

    Add roboto fontface

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from 'app/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./dependencies";
+import "./index.css";
+import App from "app/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));

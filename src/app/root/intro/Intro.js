8c940b9b252547fa14fd02d354d72befe8f143ab
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-09-18 22:39:50 -0300

    Intro draft 2

--- src/app/root/intro/Intro.js
+++ src/app/root/intro/Intro.js
@@ -2,26 +2,60 @@ import React from "react";
 import styled from "styled-components";
 import { translate } from "react-i18next";
 
+import Badge, { BadgeLink } from "components/Badge";
+
 const url = "https://avatars0.githubusercontent.com/u/8015037?s=200&v=4";
+
+const width = 660;
 
 const Container = styled.header`
   display: flex;
   align-items: center;
+
+  @media screen and (max-width: ${width}px) {
+    flex-direction: column;
+  }
 `;
 
 const Avatar = styled.img`
   border-radius: 50%;
-  margin-right: 15px;
+  margin: 15px;
+`;
+
+const Data = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  @media screen and (max-width: ${width}px) {
+    text-align: center;
+  }
 `;
 
 const Name = styled.h1`
   font-size: 1.7em;
+  margin: 0;
+  @media screen and (max-width: ${width}px) {
+    font-size: 1.4em;
+  }
 `;
 
-const Data = styled.div``;
+const Info = styled.p`
+  display: flex;
+  margin: 0;
+  flex-wrap: wrap;
+  @media screen and (max-width: ${width}px) {
+    justify-content: center;
+  }
+`;
 
-const Info = styled.p``;
-const Contact = styled.address``;
+const Contact = styled.address`
+  display: flex;
+  margin: 0;
+  flex-wrap: wrap;
+  @media screen and (max-width: ${width}px) {
+    justify-content: center;
+  }
+`;
 
 const Intro = ({ t }) => {
   return (
@@ -30,10 +64,13 @@ const Intro = ({ t }) => {
       <Data>
         <Name>{t("name")}</Name>
         <Info>
-          {t("nationality")}, {t("age", { years: 27 })}, {t("reside")}
+          <Badge>{t("nationality")}</Badge>
+          <Badge>{t("age", { years: 27 })}</Badge>
+          <Badge>{t("reside")}</Badge>
         </Info>
         <Contact>
-          <a href={`mailto:${t("email")}`}>{t("email")}</a>, {t("phone")}
+          <BadgeLink href={`mailto:${t("email")}`}>{t("email")}</BadgeLink>
+          <Badge>{t("phone")}</Badge>
         </Contact>
       </Data>
     </Container>

62b03ceb3d34fbdb7aa2f3f7b3941de4b8d8557a
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-09-18 21:34:14 -0300

    intro draft

--- src/app/root/intro/Intro.js
+++ src/app/root/intro/Intro.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { translate } from "react-i18next";
 
 const url = "https://avatars0.githubusercontent.com/u/8015037?s=200&v=4";
 
@@ -13,15 +14,30 @@ const Avatar = styled.img`
   margin-right: 15px;
 `;
 
-const Name = styled.h1``;
+const Name = styled.h1`
+  font-size: 1.7em;
+`;
 
-const Intro = props => {
+const Data = styled.div``;
+
+const Info = styled.p``;
+const Contact = styled.address``;
+
+const Intro = ({ t }) => {
   return (
     <Container>
-      <Avatar src={url} alt="Avatar" />
-      <Name>Leandro Hinckel Silveira</Name>
+      <Avatar src={url} alt={t("avatar")} />
+      <Data>
+        <Name>{t("name")}</Name>
+        <Info>
+          {t("nationality")}, {t("age", { years: 27 })}, {t("reside")}
+        </Info>
+        <Contact>
+          <a href={`mailto:${t("email")}`}>{t("email")}</a>, {t("phone")}
+        </Contact>
+      </Data>
     </Container>
   );
 };
 
-export default Intro;
+export default translate(["intro"])(Intro);

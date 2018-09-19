fe4ed60812d7a4e8a643f53aa160d29b3a504c75
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-09-18 23:29:11 -0300

    Allow display an Icon in Badge component

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,17 +1,40 @@
+import React from "react";
 import styled from "styled-components";
 
-const baseBadge = element => styled(element)`
+import Icon from "./Icon";
+
+const Container = styled.span`
   font-style: normal;
-  text-decoration: none;
+  display: flex;
+  align-items: center;
   background-color: #ccc;
   border-radius: 25px;
   padding: 8px;
-  color: inherit;
   margin: 5px;
 `;
 
-const Badge = baseBadge("span");
+const Link = styled.a`
+  text-decoration: none;
+  color: inherit;
+`;
 
-export const BadgeLink = baseBadge("a");
+const BadgeIcon = styled(Icon)`
+  margin-right: 5px;
+`;
+
+const BadgeContent = styled.span``;
+
+const Badge = ({ icon, href, children }) => {
+  const content = (
+    <Container>
+      {!!icon && <BadgeIcon icon={icon} />}
+      <BadgeContent>{children}</BadgeContent>
+    </Container>
+  );
+  if (href) {
+    return <Link href={href}>{content}</Link>;
+  }
+  return content;
+};
 
 export default Badge;

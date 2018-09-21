a1c5c2522288b2588c2a419c278b50cbf7fa9ecf
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-09-20 21:13:32 -0300

    Add about section

---
+++ src/app/root/about/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import { translate } from "react-i18next";
+
+const Container = styled.div`
+  margin-top: 24px;
+`;
+
+const Paragraph = styled.p``;
+
+const About = ({ t }) => (
+  <Container>
+    <Paragraph>{t("about")}</Paragraph>
+  </Container>
+);
+
+export default translate(["about"])(About);
